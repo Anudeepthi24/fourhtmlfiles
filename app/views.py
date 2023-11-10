d60b3ddad845a9e377e2bed54f9a93c36002eb0e
Anudeepthi24 <deepthidharmavarapu@gmail.com>
2023-11-10 11:27:46 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def anu(request):
+    return render(request,'anu.html')
+
+def deepthi(request):
+    return render(request,'deepthi.html')
+
+def ajay(request):
+    return render(request,'ajay.html')
+
+def raj(request):
+    return render(request,'raj.html')
